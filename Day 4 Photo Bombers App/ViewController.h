965dd13c84f1e2a191a4edeb697c81c8ab4b5a88
Daniyar Mukhanov <mukhanov.daniyar@gmail.com>
2015-06-18 11:19:45 +0600

    Initial Commit

---
+++ Day 4 Photo Bombers App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Day 4 Photo Bombers App
+//
+//  Created by Daniyar Mukhanov on 6/18/15.
+//  Copyright (c) 2015 Daniyar Mukhanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
